e16868475f562c549a77d7e578586d4f1537f0aa
himanshuabd17 <himanshu12061998@gmail.com>
2023-02-19 22:56:57 +0530

    swagger assignment

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const {noteRouter}=require("./routes/notes.routes");
 const { authenticate } = require("./middlewares/authenticate.middleware");
 
 
+//step 2 (importing the swaggerjsdoc and swagger-ui-express)(step 1 was installing them)
+const swaggerUI=require("swagger-ui-express");
+const swaggerJsDoc=require("swagger-jsdoc");
+
+
 //getting this cors for cross origin resource sharing 
 const cors=require("cors");
 
@@ -18,6 +23,36 @@ const app=express();
 app.use(cors());
 
 app.use(express.json());
+
+
+//step 3 (writing these specifications)
+//giving the specifications for the swagger UI OAS
+const options={
+    definition:{
+        openapi:"3.0.0",
+        info:{
+             title:"learning swagger for the first time",
+             version:"1.0.0"
+        },
+        server:[
+            {
+                //the link of the url from where the server is going to be connected.
+                url:"http://localhost:9000"
+                //url:"https://fierce-lime-jacket.cyclic.app"
+            }
+        ]
+    },
+    //it has the link of the file where the  apis are present
+    apis:["./routes/*.js"]
+}
+
+
+//step 4 (making the swaggerspecs which makes the UI)
+const swaggerSpecs=swaggerJsDoc(options)
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpecs))
+
+
+
 
 //this is a middleware for the router ,if someone hits at user, the userRouter middleware is hit
 app.use("/users",userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,29 +8,6 @@ userRouter.use(express.json());
 const {UserModel}=require("../model/user.model");
 
 
-//the router to register the user ,encrypting the password
-userRouter.post("/register",async (req,res)=>{
-   // const userData=req.body;
-    const {name,email,password}=req.body;
-    try{
-
-        bcrypt.hash(password, 5 ,async (err, hash)=> {
-            // Store hash in your password DB.
-            if(err) res.send(err.message);
-            else {
-                const user=new UserModel({name,email,password:hash});
-                await user.save();
-                res.send({"msg":"new user has been registered"})
-            }
-        });
-       
-    }
-    catch(err){
-        res.send({"msg":"something went wrong","err":err.message});
-    }
-})
-
-
 
 //logging in by decrypting the password
 userRouter.post("/login",async(req,res)=>{
@@ -65,6 +42,190 @@ userRouter.post("/login",async(req,res)=>{
     }
     
 })
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          properties:
+ *              _id:
+ *                  type: string
+ *                  description: The auto-generated id of user
+ *              name:
+ *                  type: string
+ *                  description: The username
+ *              email:
+ *                  type: string
+ *                  description: The user email
+ *              age:
+ *                  type: integer
+ *                  description: The user's age
+ *                     
+ *    
+ */
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      summary: This will get all the user data from the database
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: The list of all the users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          item:
+ *                              $ref: "#/components/schemas/User"
+ *                
+ *                                  
+ *                          
+ *  
+ */
+userRouter.get("/",async(req,res)=>{
+    const users=await UserModel.find();
+    res.send(users);
+})
+
+
+
+/**
+* @swagger
+* /users/create:
+*   post:
+*       summary: To post the details of a new user
+*       tags: [Users]
+*       requestBody:
+*           required: true
+*           content:
+*               application/json:
+*                   schema:
+*                       $ref: '#/components/schemas/User'
+*       responses:
+*           200:
+*               description: The user was successfully registered
+*               content:
+*                   application/json:
+*                       schema:
+*                           $ref: '#/components/schemas/User'
+*           500:
+*               description: Some server error
+*/
+
+userRouter.post("/create",async (req,res)=>{
+    // const userData=req.body;
+     const {name,email,password}=req.body;
+     try{
+ 
+         bcrypt.hash(password, 5 ,async (err, hash)=> {
+             // Store hash in your password DB.
+             if(err) res.send(err.message);
+             else {
+                 const user=new UserModel({name,email,password:hash});
+                 await user.save();
+                 res.send({"msg":"new user has been registered"})
+             }
+         });
+        
+     }
+     catch(err){
+         res.send({"msg":"something went wrong","err":err.message});
+     }
+ })
+ 
+ 
+
+
+/**
+ * @swagger
+ * /users/update/{id}:
+ *  patch:
+ *      summary: This will get all the user data from the database
+ *      tags: [Users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: string
+ *              required: true
+ *              description: The user id  
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/User"
+ *      responses:
+ *          200:
+ *              description: the user details have been updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                        $ref: "#/components/schemas/User"
+ *          404:
+ *              description: the user was not found
+ *                
+ *                                  
+ *                          
+ *  
+ */
+
+userRouter.patch("/update/:id",async(req,res)=>{
+    const id=req.params.id;
+    const payload=req.body;
+    await UserModel.findByIdAndUpdate({_id:id},payload)
+    res.send({"msg":"User details have been updated"});
+})
+
+
+
+//delete the user
+userRouter.patch("/delete/:id",async(req,res)=>{
+    const id=req.params.id;
+    await UserModel.findByIdAndDelete({_id:id})
+    res.send({"msg":"User details have been updated"});
+})
+
+
+
+/**
+ * @swagger
+ * /users/delete/{id}:
+ *  delete:
+ *      summary: This will get all the user data from the database
+ *      tags: [Users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *              type: string
+ *              required: true
+ *              description: The user id  
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/User"
+ *      responses:
+ *          200:
+ *              description: the user details have been updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                        $ref: "#/components/schemas/User"
+ *          404:
+ *              description: the user was not found
+ *                
+ *                                  
+ *                          
+ *  
+ */
+
 
 module.exports={
     userRouter
